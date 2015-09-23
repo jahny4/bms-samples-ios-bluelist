a40cc0812eb84195c1935a8c6d30c425a24277f0
Kevan Holdaway <kholdaway@us.ibm.com>
2015-09-23 11:34:57 -0400

    Improved the HTTP interceptor code to handle 401/403 status code.

--- bluelist-objective-c/bluelist-objective-c/CloudantHttpInterceptor.m
+++ bluelist-objective-c/bluelist-objective-c/CloudantHttpInterceptor.m
@@ -30,7 +30,7 @@
 {
     self = [super self];
     if(self){
-        _sessionCookie = sessionCookie;
+        _sessionCookie = nil;//sessionCookie;
         _refreshSessionCookieUrl = refreshSessionCookieUrl;
     }
     return self;
@@ -39,22 +39,78 @@
 -(CDTHTTPInterceptorContext*)interceptRequestInContext:(CDTHTTPInterceptorContext *)context
 {
     if(![context.request valueForHTTPHeaderField:COOKIE_HEADER]){
-        [context.request addValue:self.sessionCookie forHTTPHeaderField:COOKIE_HEADER];
+        if(!self.sessionCookie){
+            dispatch_semaphore_t sessionCookieSemaphore = dispatch_semaphore_create(0);
+            
+            [self obtainSessionCookie:^(NSError *error) {
+                [context.request addValue:self.sessionCookie forHTTPHeaderField:COOKIE_HEADER];
+                context.shouldRetry = YES;
+                dispatch_semaphore_signal(sessionCookieSemaphore);
+            }];
+            
+            dispatch_semaphore_wait(sessionCookieSemaphore, DISPATCH_TIME_FOREVER);
+        }else{
+            [context.request addValue:self.sessionCookie forHTTPHeaderField:COOKIE_HEADER];
+        }
     }
     return context;
 }
 
 -(CDTHTTPInterceptorContext*)interceptResponseInContext:(CDTHTTPInterceptorContext *)context
 {
-    //FIXMEKH handle 401.
-//    if(context.response.statusCode == 401 || context.response.statusCode == 403){
-//        [[IMFAuthorizationManager sharedInstance]obtainAuthorizationHeaderWithCompletionHandler:^(IMFResponse *response, NSError *error) {
-//            
-//            context.request addValue:<#(nonnull NSString *)#> forHTTPHeaderField:<#(nonnull NSString *)#>
-//        }];
-//        context.shouldRetry = YES;
-//    }
+    if(context.response.statusCode == 401 || context.response.statusCode == 403){
+        dispatch_semaphore_t sessionCookieSemaphore = dispatch_semaphore_create(0);
+        
+        [self obtainSessionCookie:^(NSError *error) {
+            [context.request addValue:self.sessionCookie forHTTPHeaderField:COOKIE_HEADER];
+            context.shouldRetry = YES;
+            dispatch_semaphore_signal(sessionCookieSemaphore);
+        }];
+        
+        dispatch_semaphore_wait(sessionCookieSemaphore, DISPATCH_TIME_FOREVER);
+    }
     return context;
+}
+
+-(void) obtainSessionCookie: (void(^)(NSError *error)) completionHandler
+{
+    [[IMFAuthorizationManager sharedInstance] obtainAuthorizationHeaderWithCompletionHandler:^(IMFResponse *response, NSError *error) {
+        if(error){
+            completionHandler(error);
+        }else{
+            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:self.refreshSessionCookieUrl];
+            request.HTTPMethod = @"POST";
+            [request addValue:[[IMFAuthorizationManager sharedInstance]cachedAuthorizationHeader] forHTTPHeaderField:@"Authorization"];
+            
+            
+            [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+                if(error){
+                    completionHandler(error);
+                    return;
+                }
+                
+                NSInteger httpStatus = ((NSHTTPURLResponse*)response).statusCode;
+                if(httpStatus != 200){
+                    completionHandler([NSError errorWithDomain:@"BlueList" code:42 userInfo:@{NSLocalizedDescriptionKey : [NSString stringWithFormat:@"Invalid HTTP Status %ld.  Check NodeJS application on Bluemix", httpStatus]}]);
+                    return;
+                }
+                
+                if(data){
+                    NSError *jsonError = nil;
+                    NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData: data options:0 error: &jsonError];
+                    if(!jsonError && jsonObject){
+                        NSString *sessionCookie = jsonObject[@"sessionCookie"];
+                        self.sessionCookie = sessionCookie;
+                        completionHandler(nil);
+                    }else{
+                        completionHandler(jsonError);
+                    }
+                }else{
+                    completionHandler([NSError errorWithDomain:@"BlueList" code:42 userInfo:@{NSLocalizedDescriptionKey : @"No JSON data returned from enroll call.  Check NodeJS application on Bluemix"}]);
+                }
+            }] resume];
+        }
+    }];
 }
 
 @end
